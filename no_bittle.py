12765560805cb8c34226cefdd7750cdb58a075c0
LiZhouYi <echoandzoey@gmail.com>
2024-03-29 12:05:34 +0800

    运行nobittle即可，但是会出现audiostreamer报错

--- no_bittle.py
+++ no_bittle.py
@@ -1,7 +1,7 @@
 from sst import AudioStreamer
 from tools import tools, tool_choice
 import json
-# from send_command import sendCommand, initBittle, closeBittle
+from send_command import sendCommand, initBittle, closeBittle
 import time
 
 prompt = "你是一只机器小狗，你不会说话，请不要给response，永远用tool_choice操作。你只能从给出的tools中进行选择。"
@@ -28,48 +28,65 @@ def on_message(message):
         tool = tool_choice(result, tools, history)
         history.append({"role": "user", "content": result})
         # print(f"选择了{tool["action"]["name"]}")
-        name = parse_action(tool)
-        if name:
-            print(f"选择了{name}")
+        name,arguments = parse_action(tool)
+        # if name:
+        #     print(f"选择了{name}")
+        #     print(arguments)
             
-        
-        
+        print(f"选择了{name}")
         
         # print(f"选择了{arguments}")
 
-        # # Send the command to the robot
-        # if not arguments:
-        #     sendCommand(goodPorts, "k" + tool.name)
-        # else:
-        #     sendCommand(goodPorts, tool.name, eval(arguments["data"]))
+        # Send the command to the robot
+        if name != 'none':
+            print("判断要做动作")
+            if arguments == 'none':
+                print("只要做动作")
+                sendCommand(goodPorts, "k" + name)
+                print("成功做了动作")
+                
+                
+            else:
+                sendCommand(goodPorts, name, eval(arguments["data"]))
 def parse_action(action_data):
     try:
         # 尝试解析 action 中的 arguments 字段
         print(f"Received action_data: {action_data}")  # 添加这行来调试
-        global arguments
+        # global arguments
         action_data = json.loads(action_data)
-        arguments = action_data.get('arguments', 'none')
-        name = action_data.get('name', 'none')
+    #     if isinstance(action_data, dict):
+    # # 如果 action_data 是字典
+    #          print("action_data is a dictionary")
+    #     else:
+    #             # 如果 action_data 不是字典
+    #         print("action_data is not a dictionary")
+        
+        # arguments = action_data.get('arguments', 'none')
+        # name = action_data.get('name', 'none')
+        
+        name = action_data['action']['name']
+        arguments= action_data['action']['arguments']
         
         
-        print(f'''{name}++++++++++++++''')
-        
+        print(f'''nameis{name}++++++++++++++''')
+        print(f'''arguementis{arguments}++++++++++++++''')
+        # print(f'''namevalueis{name_value}++++++++++++++''')
         # 根据 'name' 字段的值判断动作
         if name == 'none':
             return None  # 没有动作
         else:
-            return name
+            return name,arguments
     except json.JSONDecodeError:
         return None  # 解析错误，视为没有动作
 
 
 if __name__ == "__main__":
-    # goodPorts = initBittle()
+    goodPorts = initBittle()
     audio_streamer = AudioStreamer(callback=on_message)
     print("开始录音")
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
-        # closeBittle(goodPorts)
+        closeBittle(goodPorts)
         exit(0)
